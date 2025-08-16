1aa265bea861b42eecaca46dc094e67e3e2ac3a5
Zunda <mailme@jacobhaussman.online>
2025-08-16 19:53:23 -0400

    add enemy and room files

---
+++ game/enemy.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name Enemy
+
+var sprite: String
+var display_name: String
+var speed: int
+var health: int
---
+++ game/room.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name Room
+
+var sprite: String
+var display_name: String
+var cooldown_seconds: int
+var damage: int
