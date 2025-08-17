0716f252471dcb7af41ff126909d66bf6ee04131
Alice <cheezyanimations@gmail.com>
2025-08-17 03:24:16 -0400

    projectile movement

--- game/projectile.gd
+++ game/projectile.gd
@@ -2,18 +2,36 @@ extends Node2D
 
 @export var damage = 50
 
-@export var speed = 5
-@export var fall_speed = 1
-@export var gravity = 1
+@export var speed = 500.0
 
 var vertical_speed = 0
 var horizontal_speed = 0
+var time = 0
+var enemy_position_x = 0
+var enemy_position_y = 0
+var origin_x = 0
+var origin_y = 0
 
+func y_pos(px: float, py: float, ex: float, ey: float, x: float):
+	var d = (py - ey) / (px - ex)
+	var inverse_speed = (-1.0 / speed)
+	return -inverse_speed * x * x + inverse_speed * x * px + inverse_speed * x * ex + d * x - inverse_speed * px * ex - ex * d + ey
+	
 func aim(enemy: Enemy):
-	vertical_speed = speed
-	horizontal_speed = speed
+	enemy_position_x = 400
+	enemy_position_y = 300
+	origin_x = 0
+	origin_y = 300
+	
+	self.global_position.x
+	self.global_position.y
 
 func _process(delta: float) -> void:
-	self.global_position.x += speed
-	self.global_position.y -= vertical_speed
-	vertical_speed += gravity
+	time += delta
+		
+	self.global_position.x = time * speed
+	self.global_position.y = y_pos(origin_x, origin_y, enemy_position_x, enemy_position_y, time * speed)
+
+	var string = "x: %s, y: %s"
+	var formatted_string = string % [self.global_position.x, self.global_position.y]
+	print(formatted_string)
