d46266fa24a6105f2e7fbd0e01c46b72f01c6795
Zunda <mailme@jacobhaussman.online>
2025-08-17 11:36:37 -0400

    projectiles shoot where they should go

--- game/projectile.gd
+++ game/projectile.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 @export var damage = 50
 
-@export var speed = 10
+@export var speed = 1000
 @export var gravity = 2.0
 
 var time = 0
 var enemy_position: Vector2 = Vector2.ZERO
 var origin: Vector2 = Vector2.ZERO
+
+var angle: float
 
 func y_pos(p: Vector2, e: Vector2, x: float):
 	var d = (p.y - e.y) / (p.x - e.x)
@@ -20,9 +22,10 @@ func y_pos(p: Vector2, e: Vector2, x: float):
 func aim(spawnpoint: Vector2, enemy: Enemy):
 	enemy_position = enemy.global_position
 	origin = spawnpoint
+	
+	angle = (enemy_position - origin).angle()
 
 func _process(delta: float) -> void:
 	time += delta
-		
-	self.global_position.x = time * speed + origin.x
-	self.global_position.y = y_pos(origin, enemy_position, time * speed)
+	self.global_position.x += cos(angle) * speed * delta
+	self.global_position.y += sin(angle) * speed * delta
