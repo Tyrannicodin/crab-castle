2fe05887d4396d5c72bbb8d350717b54a11f074e
Alice <cheezyanimations@gmail.com>
2025-08-17 15:24:41 -0400

    fix projectile code again

--- game/projectile.gd
+++ game/projectile.gd
@@ -2,30 +2,23 @@ extends Node2D
 
 @export var damage = 50
 
-@export var speed = 1000
-@export var gravity = 2.0
+@export var speed = 500.0
 
 var time = 0
-var enemy_position: Vector2 = Vector2.ZERO
-var origin: Vector2 = Vector2.ZERO
+var enemy_position: Vector2i = Vector2.ZERO
+var origin: Vector2i = Vector2.ZERO
 
-var angle: float
-
-func y_pos(p: Vector2, e: Vector2, x: float):
-	var d = (p.y - e.y) / (p.x - e.x)
-	
-	# f''(x) = gravitational constant
-	var y_scale = (gravity / 2.0) * (1.0 / speed)
-	
-	return y_scale * x * x - y_scale * x * p.x - y_scale * x * e.x + d * x + y_scale * p.x * e.x - e.x * d + e.y
+func y_pos(px: float, py: float, ex: float, ey: float, x: float):
+	var d = (py - ey) / (px - ex)
+	var inverse_speed = (-1.0 / speed)
+	return -inverse_speed * x * x + inverse_speed * x * px + inverse_speed * x * ex + d * x - inverse_speed * px * ex - ex * d + ey
 	
 func aim(spawnpoint: Vector2, enemy: Enemy):
 	enemy_position = enemy.global_position
 	origin = spawnpoint
-	
-	angle = (enemy_position - origin).angle()
-
+		
 func _process(delta: float) -> void:
 	time += delta
-	self.global_position.x += cos(angle) * speed * delta
-	self.global_position.y += sin(angle) * speed * delta
+		
+	self.global_position.x = time * speed
+	self.global_position.y = y_pos(origin.x, origin.y, enemy_position.x, enemy_position.y, time * speed)
