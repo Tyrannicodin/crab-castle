a7eeedf60b555dcc023be17b5147c330784a5e0c
Alice <cheezyanimations@gmail.com>
2025-08-17 15:49:02 -0400

    changes

--- game/projectile.gd
+++ game/projectile.gd
@@ -3,22 +3,30 @@ extends Node2D
 @export var damage = 50
 
 @export var speed = 500.0
+@export var velocity = 1.5
 
 var time = 0
 var enemy_position: Vector2i = Vector2.ZERO
 var origin: Vector2i = Vector2.ZERO
 
-func y_pos(px: float, py: float, ex: float, ey: float, x: float):
-	var d = (py - ey) / (px - ex)
-	var inverse_speed = (-1.0 / speed)
-	return -inverse_speed * x * x + inverse_speed * x * px + inverse_speed * x * ex + d * x - inverse_speed * px * ex - ex * d + ey
+func y_pos(ex: float, ey: float, x: float):
+	var d = ey / ex
+	var inverse_speed = (1.0 / speed)
+	return inverse_speed * x * x - inverse_speed * x * ex + d * x - ex * d + ey
+	
+func y_pos_dx(ex: float, ey: float, x: float):
+	var d = ey / ex
+	var inverse_speed = (1.0 / speed)
+	return 2 * inverse_speed * x - inverse_speed * ex + d
 	
 func aim(spawnpoint: Vector2, enemy: Enemy):
-	enemy_position = enemy.global_position
 	origin = spawnpoint
-		
+	enemy_position = Vector2(enemy.global_position.x - enemy.speed * speed / 100.0, enemy.global_position.y)
+	var velocity = y_pos_dx(enemy_position.x, enemy_position.y, 0)
+	
+			
 func _process(delta: float) -> void:
 	time += delta
-		
-	self.global_position.x = time * speed
-	self.global_position.y = y_pos(origin.x, origin.y, enemy_position.x, enemy_position.y, time * speed)
+				
+	self.global_position.x = origin.x + time * speed
+	self.global_position.y = y_pos(enemy_position.x, enemy_position.y, time * speed) + origin.y
