8bd50250b67f84278c9e46e67769d6b372ffa74a
Zunda <mailme@jacobhaussman.online>
2025-08-16 21:00:14 -0400

    add seagull speed

--- game/enemy.gd
+++ game/enemy.gd
@@ -4,3 +4,6 @@ class_name Enemy
 @export var display_name: String
 @export var speed: int
 @export var health: int
+
+func _process(delta: float) -> void:
+	self.position.x -= delta * speed
--- game/game.gd
+++ game/game.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
-var rooms = [
-]
+
+func spawn_enemy():
+	# This will be what layer of the tower the enemy
+	# spawns on once thats possible
+	var layer = 1
+	var seagull: Enemy = load("res://game/enemies/Seagull.tscn").instantiate()
+	$EnemySpawnPoint.add_child(seagull)
+	print(seagull.global_position)
+
+func _ready() -> void:
+	spawn_enemy()
