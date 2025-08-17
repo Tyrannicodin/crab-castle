851e9c1d81aaddcd508af2778ae1667d8f45934d
Zunda <mailme@jacobhaussman.online>
2025-08-16 21:05:52 -0400

    adjustments to the seagull

--- game/enemy.gd
+++ game/enemy.gd
@@ -5,5 +5,9 @@ class_name Enemy
 @export var speed: int
 @export var health: int
 
+var time = 0.0
+
 func _process(delta: float) -> void:
+	time += delta
 	self.position.x -= delta * speed
+	self.position.y = 10 * sin(2 * time)
