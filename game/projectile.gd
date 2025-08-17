3aa18365d0258a5aeba7a3668f4ef78f35c22d26
Alice <cheezyanimations@gmail.com>
2025-08-17 05:42:03 -0400

    speed fixes

--- game/projectile.gd
+++ game/projectile.gd
@@ -2,10 +2,9 @@ extends Node2D
 
 @export var damage = 50
 
-@export var speed = 1000.0
+@export var speed = 100.0
+@export var gravity = 2.0
 
-var vertical_speed = 0
-var horizontal_speed = 0
 var time = 0
 var enemy_position_x = 0
 var enemy_position_y = 0
@@ -14,9 +13,11 @@ var origin_y = 0
 
 func y_pos(px: float, py: float, ex: float, ey: float, x: float):
 	var d = (py - ey) / (px - ex)
-	var gravity = speed
-	var inverse_gravity = -1.0 / gravity
-	return -inverse_gravity * x * x + inverse_gravity * x * px + inverse_gravity * x * ex + d * x - inverse_gravity * px * ex - ex * d + ey
+	
+	# f''(x) = gravitational constant
+	var y_scale = (gravity / 2.0) * (1.0 / speed)
+	
+	return y_scale * x * x - y_scale * x * px - y_scale * x * ex + d * x + y_scale * px * ex - ex * d + ey
 	
 func aim(enemy: Enemy):
 	enemy_position_x = 400
@@ -32,7 +33,3 @@ func _process(delta: float) -> void:
 		
 	self.global_position.x = time * speed
 	self.global_position.y = y_pos(origin_x, origin_y, enemy_position_x, enemy_position_y, time * speed)
-
-	var string = "x: %s, y: %s"
-	var formatted_string = string % [self.global_position.x, self.global_position.y]
-	print(formatted_string)
