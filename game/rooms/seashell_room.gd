ac66a2f3329efdb0d43415f07c7da98f81c14e3a
Zunda <mailme@jacobhaussman.online>
2025-08-18 00:28:55 -0400

    add missing files

---
+++ game/rooms/seashell_room.gd
@@ -0,0 +1,9 @@
+extends "res://game/room.gd"
+
+
+func _on_trigger() -> void:
+	var buff_rooms: Array[Room] = game.find_adjascent_rooms(self).values()
+
+	for room in buff_rooms:
+		if room:
+			room.extra_projectiles_for_next_shot += 1
