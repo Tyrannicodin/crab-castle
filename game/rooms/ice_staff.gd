8203861ff7094709503cc30b7358da778ee785a7
Zunda <mailme@jacobhaussman.online>
2025-08-18 02:04:45 -0400

    make rooms look better

--- game/rooms/ice_staff.gd
+++ game/rooms/ice_staff.gd
@@ -1,6 +1,6 @@
 extends "res://game/room.gd"
 
-var projectile = load("res://game/rooms/IceStaffProjectile.tscn")
+var projectile = load("res://game/rooms/FreezerProjectile.tscn")
 
 func _on_trigger() -> void:
 	fire_projectiles_above_enemy(projectile, 1)
