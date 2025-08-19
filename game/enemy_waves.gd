09a9cba394608017c6dc4b0a6cb2f4195b0dbc02
Zunda <mailme@jacobhaussman.online>
2025-08-19 01:22:46 -0400

    ease hit effect

--- game/enemy_waves.gd
+++ game/enemy_waves.gd
@@ -4,9 +4,9 @@ var Seagull = load("res://assets/resources/enemies/seagull.tres")
 
 var waves = [
 	# Wave 1
-	func(): return [
-		[Seagull]
-	],
+	#func(): return [
+	#	[Seagull]
+	#],
 	# Wave 2
 	func(): return [
 		[Seagull, Seagull, Seagull],
