33f376d7c89f006ad29c89164ded642a5c67e212
Zunda <mailme@jacobhaussman.online>
2025-08-20 20:53:41 -0400

    sorta get the claw working

--- game/tooltip.gd
+++ game/tooltip.gd
@@ -4,3 +4,7 @@ class_name Tooltip
 func set_description(n: String, desc: String):
 	$VBoxContainer/name.text = n
 	$VBoxContainer/description.text = desc
+
+func show_cost(value: int):
+	$VBoxContainer/Cost.show()
+	$VBoxContainer/Cost/Label.text = str(value)
