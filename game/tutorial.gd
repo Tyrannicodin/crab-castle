9d07275d04b6c0cdfc8a66855b1215c2cb7fdd3a
Zunda <mailme@jacobhaussman.online>
2025-08-21 14:25:51 -0400

    undo tutorial changes

--- game/tutorial.gd
+++ game/tutorial.gd
@@ -1,42 +1,28 @@
 extends CanvasLayer
 
-@onready var textBox = $TutorialTextBox
-
-var text = [
-	"The seagulls are attacking! We have to build a castle!",
-	"Drag your weapon onto the platform.",
-	"Press start wave and defend!",
-]
-
-@onready var tutorial_disabled = not ConfigManager.get_value("show_tutorial", true)
-var tutorial_index = 0
-
-func next_tutorial() -> void:
-	for child in get_children():
-		child.hide()
-	
-	if tutorial_disabled:
-		return
-
-	if tutorial_index >= len(text):
-		tutorial_disabled = true
-		ConfigManager.set_value("show_tutorial", false)
-		return
-
-	textBox.show()
-	textBox.text = text[tutorial_index]
-	if get_node_or_null("Tutorial" + str(tutorial_index)):
-		get_node("Tutorial" + str(tutorial_index)).show()
-	tutorial_index += 1
+var tutorial_disabled = false
 
 func _ready() -> void:
-	next_tutorial()
+	if tutorial_disabled: return
+	$One.show()
 
-func _on_upgrade_ui_upgrade_selected(_room: Room) -> void:
-	next_tutorial()
 
-func _on_tower_room_placed(_room: int) -> void:
-	next_tutorial()
+func _on_upgrade_ui_upgrade_selected(room: Room) -> void:
+	if tutorial_disabled: return
+	$One.hide()
+	$Two.show()
+
+
+func _on_tower_room_placed(room: int) -> void:
+	if tutorial_disabled: return
+	$One.hide()
+	$Two.hide()
+	$Three.show()
+
 
 func _on_game_wave_start() -> void:
-	next_tutorial()
+	if tutorial_disabled: return
+	$One.hide()
+	$Two.hide()
+	$Three.hide()
+	tutorial_disabled = true
