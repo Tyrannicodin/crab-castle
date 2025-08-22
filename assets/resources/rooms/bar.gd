324a452530e1c16c60567bdc7dd59ebb5ab69d2d
Zunda <mailme@jacobhaussman.online>
2025-08-22 01:42:29 -0400

    fix beer asset

---
+++ assets/resources/rooms/bar.gd
@@ -0,0 +1,4 @@
+static var projectile = preload("res://game/projectiles/Bar.tscn")
+
+static func on_trigger(tower: Tower, room: Tower.RoomInstance) -> void:
+	tower.fire_projectiles(room, projectile, 1)
