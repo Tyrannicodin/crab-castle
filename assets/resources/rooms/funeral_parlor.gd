cf6e7cac475a5834c39c7698e2232ec0a673cd85
Zunda <mailme@jacobhaussman.online>
2025-08-21 23:57:30 -0400

    funeral parlor

--- assets/resources/rooms/funeral_parlor.gd
+++ assets/resources/rooms/funeral_parlor.gd
@@ -1,4 +1,19 @@
 static var projectile = preload("res://game/projectiles/Cannon.tscn")
 
 static func on_trigger(tower: Tower, room: Tower.RoomInstance) -> void:
-	room.workshop_extra_coins += 1
+	var lives_remaining = tower.game.tower_health
+	var scaling = room.funeral_parlor_scaling
+
+	if scaling > lives_remaining:
+		scaling = (lives_remaining - 1)
+
+	tower.game.tower_health -= scaling
+	room.create_flavor_text(tower, "-%d Health" % scaling)
+
+	for r: Tower.RoomInstance in tower.get_adjacent_rooms(room).values():
+		if r == null: continue
+		await tower.get_tree().create_timer(.2).timeout
+		r.funeral_parlor_extra_damage += scaling
+		r.create_flavor_text(tower, "+%d Damage" % scaling)
+
+	room.funeral_parlor_scaling += 1
--- assets/resources/rooms/workshop.gd
+++ assets/resources/rooms/workshop.gd
@@ -4,4 +4,4 @@ static func on_trigger(tower: Tower, room: Tower.RoomInstance) -> void:
 	room.workshop_extra_coins += 1
 	tower.game.money += room.workshop_extra_coins
 	room.play_sound(tower, "buff")
-	room.create_flavor_text(tower, "+d Coins" % room.workshop_extra_coins)
+	room.create_flavor_text(tower, "+%d Coins" % room.workshop_extra_coins)
