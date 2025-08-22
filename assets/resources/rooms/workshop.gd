cc3cf851a4a998a213c1ff19ed0e8c0494d291e5
Zunda <mailme@jacobhaussman.online>
2025-08-22 00:26:15 -0400

    working the shop

--- assets/resources/rooms/workshop.gd
+++ assets/resources/rooms/workshop.gd
@@ -1,7 +1,7 @@
 static var projectile = preload("res://game/projectiles/Cannon.tscn")
 
 static func on_trigger(tower: Tower, room: Tower.RoomInstance) -> void:
-	room.workshop_extra_coins += 1
-	tower.game.money += room.workshop_extra_coins
+	room.workshop_extra_coins += .2
+	tower.game.money += floor(room.workshop_extra_coins)
 	room.play_sound(tower, "buff")
-	room.create_flavor_text(tower, "+%d Coins" % room.workshop_extra_coins)
+	room.create_flavor_text(tower, "+%d Coins" % floor(room.workshop_extra_coins))
