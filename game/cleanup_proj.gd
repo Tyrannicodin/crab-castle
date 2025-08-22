5215120b3c0b7f4a77f1df3b3b4cb72649219c30
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2025-08-22 15:31:41 +0100

    Don't clean up instant kill projectiles

--- game/cleanup_proj.gd
+++ game/cleanup_proj.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
 func _on_area_entered(area):
-	if area.get_parent() is Projectile:
+	if area.get_parent() is Projectile and not area.get_parent().instant_kill:
 		area.get_parent().queue_free()
