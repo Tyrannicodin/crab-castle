207c9e3dc30f87bf5c1de0d33b66d9bfce7382b6
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2025-08-22 13:28:20 +0100

    Buff nuke

--- assets/resources/rooms/nuke.gd
+++ assets/resources/rooms/nuke.gd
@@ -1,5 +1,4 @@
 static var projectile = preload("res://game/projectiles/Nuke.tscn")
 
 static func on_trigger(tower: Tower, room: Tower.RoomInstance) -> void:
-	room.bonus_projectiles = 0
 	tower.fire_projectiles(room, projectile, 1)
